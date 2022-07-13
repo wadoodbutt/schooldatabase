01983b6f9792aa327f7fa61be80a2a35dd75a544
wadoodbutt <109189695+wadoodbutt@users.noreply.github.com>
2022-07-13 15:47:07 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,9 @@
+/* Simple driver class that utilizes the other classes to create the
+ * database itself.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.mainInterface();
+    }
+}
